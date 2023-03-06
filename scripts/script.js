b014077b7e1cf0d196529eabc9df24f03290336d
mariachernyshova <mary.chernishova@mail.ru>
2023-03-07 00:24:39 +0300

    add: открытие модалки по нажатию на наименование бургера в меню

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,27 @@
+/* Открытие и закрытие корзины */
 let catalogOrder = document.querySelector('.catalog__order');
-
 let catalogOrderButton = document.querySelector('.order__wrap-title');
 catalogOrderButton.onclick = function() {
     catalogOrder.classList.toggle('order_open');
   
 };
-
-
 let orderCloseButton = document.querySelector('.order__close');
 orderCloseButton.addEventListener('click', () => {
     catalogOrder.classList.remove('order_open');
+});
+
+/* Модалка карточки*/
+let modalProduct = document.querySelector('.modal_product');
+let productDetailButtonList = document.querySelectorAll('.product__detail');
+productDetailButtonList.forEach(btn => {
+    btn.addEventListener('click', () => {
+        modalProduct.classList.add('modal__open');
+    });
+});
+
+/* Закрытие модалок*/
+let openModal = document.querySelector('.modal_product');
+let modalCloseButton = document.querySelector('.modal__close');
+modalCloseButton.addEventListener('click', () => {
+    openModal.classList.remove('modal__open');
 });
